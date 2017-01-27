e3e2dbc35451cbc1e9a7579b2b3df8cf57cee560
Conor ONeill <conor@conoroneill.com>
2017-01-27 14:17:04 +0000

    first working version that dumps data to console

--- README.md
+++ README.md
@@ -1,13 +1,52 @@
-The [Yunmai weighing scales](http://www.iyunmai.com/us/light/) is an impressive inexpensive smart scales with Bluetooth. I bought mine from [GearBest](http://www.gearbest.com/monitoring-testing/pp_332025.html). If you install their app on your phone, it will connect to the scales and save all of your data, each time you weigh yourself. If you are on Android, you can opt for the Yunmai to save the data to [Google Fit](https://fit.google.com/fit/). You obviously need to have Google Fit installed too. 
-
-However, the weight data that is saved doesn't seem to be visible (yet) in either the Google Fit Android App or the Web App. The data is there and is accessible using the Fit APIs.
+# Intro
+The [Yunmai weighing scales](http://www.iyunmai.com/us/light/) is an impressive inexpensive smart scales with Bluetooth. I bought mine from [GearBest](http://www.gearbest.com/monitoring-testing/pp_332025.html). If you install their app on your phone, it will connect to the scales and save all of your data, each time you weigh yourself. If you are on Android, you can opt for the Yunmai to save the data to [Google Fit](https://fit.google.com/fit/). You obviously need to have Google Fit installed too.
 
 This App will access your weight data via the Google API and expose it on an end-point that can then be a source for IFTTT, so you can send the data to any target supported by IFTTT. The data will probably also be persisted locally so you have a local backup (TBD).
-
-First step is just to get the OAuth flow working. Then we can worry about sending the data elsewhere.
 
 This is a work in progress.
 
 Note that Yunmai can also send to Fitbit which may be an easier route in the end. TBC.
 
-Also need to check why only weight is sent, considering how much data the Android App actually records.
+## Setting it up for yourself (instructions from Google Quickstart)
+* Install [Node.js](https://nodejs.org/en/) and [Git](https://git-scm.com/)
+* Use [this wizard](https://console.developers.google.com/start/api?id=fitness) to create or select a project in the Google Developers Console and automatically turn on the Fitness API. Click Continue, then Go to credentials.
+* On the Add credentials to your project page, click the Cancel button.
+* At the top of the page, select the OAuth consent screen tab. Select an Email address, enter a Product name if not already set, and click the Save button.
+* Select the Credentials tab, click the Create credentials button and select OAuth client ID.
+* Select the application type Other, enter the name "Yunmai Fitness", and click the Create button.
+* Click OK to dismiss the resulting dialog.
+* Click the file_download (Download JSON) button to the right of the client ID.
+* Then in a console
+
+```
+git clone https://github.com/conoro/yunmai-google-fit-ifttt
+cd yunmai-google-fit-ifttt
+npm install googleapis --save
+npm install google-auth-library --save
+```
+* Then copy the downloaded JSON file to that working directory and rename it client_secret.json
+* Run the code with
+
+```
+node index.js
+```
+
+* You'll be presented with a URL. Paste that into a browser and give the permission requested on the screen that appears
+* Copy the code that you are given back into the console
+* You shouldn't need to do those two steps again. From now on, you can just run "node index.js" every time you want to see your Yunmai data
+
+# Plan
+ I'll be iterating on this as follows:
+
+ - [x] Dump all weights and dates to console using date-range 01-01-2015 to Now
+ - [ ] Save in local CSV file
+ - [ ] Save in local IndexDB or maybe SQLite
+ - [ ] Save to Google Sheets
+ - [ ] Expose the data as simple JSON on open API end-point
+ - [ ] Expose the data in whatever format makes sense for IFTTT
+
+ Note, I probably won't bother with pagination since there is likely to only be one data point per day.
+
+# TODO
+* All of the above
+* Investigate why Yunmai only saves weight and not all the other data from the scales
---
+++ index.js
@@ -0,0 +1,128 @@
+/* yunmai-google-fit-ifttt
+
+ Copyright Conor O'Neill 2017 (conor@conoroneill.com)
+ Portions Copyright Google
+
+ This simple Node.js app accesses your weight data saved by a Yunmai weighing scales on your Google Fit/Fitness account
+
+*/
+
+var fs = require('fs');
+var readline = require('readline');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+
+// If modifying these scopes, delete your previously saved credentials
+// at ~/.credentials/fitness.googleapis.com-nodejs-quickstart.json
+var SCOPES = ['https://www.googleapis.com/auth/fitness.body.read'];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.credentials/';
+var TOKEN_PATH = TOKEN_DIR + 'fitness.googleapis.com-nodejs-quickstart.json';
+
+// Load client secrets from a local file.
+fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+  if (err) {
+    console.log('Error loading client secret file: ' + err);
+    return;
+  }
+  // Authorize a client with the loaded credentials, then call the
+  // Google Sheets API.
+  authorize(JSON.parse(content), listWeights);
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  var clientSecret = credentials.installed.client_secret;
+  var clientId = credentials.installed.client_id;
+  var redirectUrl = credentials.installed.redirect_uris[0];
+  var auth = new googleAuth();
+  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, function(err, token) {
+    if (err) {
+      getNewToken(oauth2Client, callback);
+    } else {
+      oauth2Client.credentials = JSON.parse(token);
+      callback(oauth2Client);
+    }
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
+ */
+function getNewToken(oauth2Client, callback) {
+  var authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES
+  });
+  console.log('Authorize this app by visiting this url: ', authUrl);
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question('Enter the code from that page here: ', function(code) {
+    rl.close();
+    oauth2Client.getToken(code, function(err, token) {
+      if (err) {
+        console.log('Error while trying to retrieve access token', err);
+        return;
+      }
+      oauth2Client.credentials = token;
+      storeToken(token);
+      callback(oauth2Client);
+    });
+  });
+}
+
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken(token) {
+  try {
+    fs.mkdirSync(TOKEN_DIR);
+  } catch (err) {
+    if (err.code != 'EEXIST') {
+      throw err;
+    }
+  }
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+  console.log('Token stored to ' + TOKEN_PATH);
+}
+
+/* Access Google Fit and dump all the Yunmai data that it holds for your account */
+function listWeights(auth) {
+  var fitness = google.fitness('v1');
+  var epoch = new Date().getTime()*1000000;
+
+// Date range is 01-01-2015 to Now (and set in datasetId)
+fitness.users.dataSources.datasets.get({
+  auth: auth,
+  dataSourceId: "raw%3Acom.google.weight%3Acom.yunmai.scaleen%3AGoogleFitManager-%20weight",
+  datasetId: "1420070401000000000-"+epoch,
+  userId: "me"
+}, function(err, response) {
+  if (err) {
+    console.log('The API returned an error: ' + err);
+    return;
+  }
+  for (var i = 0, len = response.point.length; i < len; i++) {
+    var weightTime = new Date(parseInt(response.point[i].modifiedTimeMillis));
+    console.log(weightTime, response.point[i].value[0].fpVal);
+  }
+});
+}
