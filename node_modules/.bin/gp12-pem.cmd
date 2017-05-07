f1b27d7500ef9ba26da1e099bbbc97d2082cfb8e
Conor ONeill <conor@conoroneill.com>
2017-05-07 20:12:08 +0100

    Git cleanup

--- node_modules/.bin/gp12-pem.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\google-p12-pem\bin\gp12-pem" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\google-p12-pem\bin\gp12-pem" %*
-)
